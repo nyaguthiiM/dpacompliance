c8c3aa0138bb99e0144124a475e691cac7f3b50c
nyagu <nyaguthiimuriuki19@gmail.com>
2020-03-13 11:56:44 -0400

    setup project

---
+++ frontend/src/App.js
@@ -0,0 +1,49 @@
+// App.js
+import React, { Component } from 'react';
+class App extends Component {
+  state = {
+    api: []
+  };
+//    This is where the magic happens
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/assets'); // fetching the data from api, before the page loaded
+
+      const api = await res.json();
+
+      this.setState({
+        api
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+            <tr><th>#</th><th>Asset</th><th>Location</th><th>Duration</th></tr>
+        {this.state.api.map(item => (
+//          <div key={item.id}>
+//            <h1>{item.asset_name}</h1>
+//            <h1><span>{item.location}</span></h1>
+//            <h1><span>{item.duration}</span></h1>
+//          </div>
+
+                <tr>
+                <td>{item.id}</td>
+                <td>{item.asset_name}</td>
+                <td>{item.location}</td>
+                <td>{item.duration}</td>
+                <td>{item.process}</td>
+                </tr>
+
+        ))}
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App
