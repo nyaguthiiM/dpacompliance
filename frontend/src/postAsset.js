d78dacd3592394f634db68116913ea5d30d72c9a
nyagu <nyaguthiimuriuki19@gmail.com>
2020-03-14 10:48:37 -0400

    ui

---
+++ frontend/src/postAsset.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+
+fetch('http://127.0.0.1:8000/api/assets/', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    firstParam: 'yourValue',
+    secondParam: 'yourOtherValue',
+  })
+})
+
